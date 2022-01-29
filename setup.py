e393b976885b18f8673f06f18d73be0d2ea6739d
dihydromonoxide <delanoleslieprof@gmail.com>
2022-01-29 12:00:26 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ c = conn.cursor()
 c.execute('''CREATE TABLE guilds (
     guild_id INTEGER PRIMARY KEY,
     settings TEXT
-    peermissions TEXT DEFAULT '[]'
+    permissions TEXT DEFAULT '[]'
     )''')
 conn.commit()
 
